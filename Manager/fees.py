1b56328af90e8a432aca750add29a1077198441c
Samuel Kuld <61301218+SamuelKuld@users.noreply.github.com>
2021-08-03 22:00:52 -0700

    Adjusted Output and Math
    Added more verbose information on the point system and adjusted the total output so that it was more accurate to what you're putting in.

--- Manager/fees.py
+++ Manager/fees.py
@@ -1,18 +1,16 @@
-from imports import *
-
 def true_fee_value(amount = 0):
-  counter = (amount * 1.005) - amount
-  ranges = ({"value" : 10.0,  "fee" : .99, "next" : 25.0}, 
+  ranges = ({"value" : 10.0,  "fee" : .99, "next" : 25.00}, 
             {"value" : 25.0, "fee" : 1.49, "next" : 50.0}, 
             {"value" : 50.0, "fee" : 1.99, "next" : 200.0}, 
             {"value" : 200.0, "fee" : 2.99, "next" : amount + 1})
+
   if amount > 200.0:
     return {"total" : amount + ((amount * .0149) * 2), "point" : 0.149}
   if amount < 5.00:
     return {"total" : 0.0, "point" : 0.0}
   if amount >= 5.00 and amount < 10.00:
-    return {"total" : amount * 1.005, "point" : .995}
+    return {"total" : amount, "point" : 1}
   for range in ranges:
     if amount >= range["value"] and amount < range["next"]:
-        return {"total" : (amount + range["fee"] * 2) + counter, 
-                "point" : abs((range["fee"] / amount) - (range["fee"] / range["value"]))}
+        return {"total" : (amount - range["fee"]), 
+                "point" : (range["fee"] / range["value"]) - (range["fee"] / amount)}
--- Manager/main.py
+++ Manager/main.py
@@ -7,12 +7,14 @@ acceptions = ["yes","1","true",
             "yuh", "yup", "yeah",
             "yah", "ya", "not no"]
 
+
 print("Rates : ",
       "\n $.99 if trade <= $10.00",
       "$1.49 if trade <= $25.00",
       "$1.99 if trade <= $50.00",
       "$2.99 if trade <= $200.00",
       "1.49% of trade if trade > $200.00", sep="\n ")
+
 
 print("\nTrade limit per day = $500")
 print("How much would you like to put in?")
@@ -22,7 +24,8 @@ amount_putting_in = float(input())
 print("Would you like to compensate for the fees?")
 if input().lower() in acceptions:
   result = true_fee_value(amount_putting_in)
-  print(f"True value = {result['total']}")
-  print(f"Value's minimum fee percentage point = {result['point'] * 10} (The closer to 1 the better)")
+  print(f"Amount going in = {result['total']}")
+  print("The point system works as \n\"   (Fee / Max Value purchaseable with minimum fee) - (Fee / Amount of dollars inputting)\"\nThis allows you to have a 0.0-1.0 amount of point room")
+  print(f"Value's minimum fee percentage point = {result['point'] * 10}")
 
 input()
